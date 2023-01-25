519ff0e0c478585460d6d7ca0a2416618aa12667
Simon <carbontwelve@users.noreply.github.com>
2023-01-25 14:27:21 +0000

    chore(#1): increase test coverage

--- tests/str-to-slug.test.js
+++ tests/str-to-slug.test.js
@@ -7,4 +7,6 @@ test('slugifies', t=>{
   t.true(slugify('hello--world') === 'hello-world')
   t.true(slugify('1 2 3') === '1-2-3')
   t.true(slugify('123') === '123')
+  t.true(slugify('11ty') === '11ty')
+  t.true(slugify('DOScember') === 'doscember')
 })
--- tests/tag-atlas.test.js
+++ tests/tag-atlas.test.js
@@ -1,35 +1,46 @@
 const test = require('ava');
-const {atlas} = require('../src/tag-atlas');
+const {TagAtlas, strToSlug, slugToStr} = require('../src/tag-atlas');
 const slugify = require('../src/str-to-slug');
 
 test('does not split titleCase or change case for ignored words', t => {
-  const tagAtlas = atlas({
+  const tagAtlas = new TagAtlas({
     slugify,
-    ignore: ['PHP', 'JS', 'JavaScript'],
-    similar: {'Game Development': ['GameDev']},
+    ignore: ['PHP', 'JS', 'JavaScript', 'MS-DOS'],
   });
 
-  t.true(tagAtlas.find('PHP').title === 'PHP');
-  t.true(tagAtlas.find('php').title === 'PHP');
-  t.true(tagAtlas.find('JavaScript').title === 'JavaScript');
-  t.true(tagAtlas.find('javascript').title === 'JavaScript');
+  const checks = {
+    'PHP':'PHP',
+    'php':'PHP',
+    'JavaScript': 'JavaScript',
+    'javascript': 'JavaScript',
+    'MS-DOS': 'MS-DOS',
+    'ms-dos': 'MS-DOS'
+  };
+
+  for (const [input, expected] of Object.entries(checks)) {
+    const result = tagAtlas.find(input);
+    t.true(result.title === expected, `[${result.title}] does not match "${expected}"`)
+  }
 });
 
 test('merges tags defined as similar', t => {
-  const tagAtlas = atlas({
+  const tagAtlas = new TagAtlas({
     slugify,
     ignore: ['PHP', 'JS', 'JavaScript'],
     similar: {'Game Development': ['GameDev']},
   });
 
-  for (const check of ['Game Development', 'GameDevelopment', 'Game Dev', 'GameDev']) {
+  const checks = ['Game Development', 'GameDevelopment', 'Game Dev', 'GameDev'];
+
+  for (const check of checks) {
     t.true(tagAtlas.find(check) !== undefined, `[${check}] should be defined`);
+
     t.true(tagAtlas.find(check).slug === 'game-development', `The slug of [${check}] should be game-development`)
   }
 });
 
 test('unknown tags return undefined', t => {
-  const tagAtlas = atlas({
+  const tagAtlas = new TagAtlas({
     slugify,
     ignore: ['PHP', 'JS', 'JavaScript'],
     similar: {'Game Development': ['GameDev']},
@@ -37,3 +48,84 @@ test('unknown tags return undefined', t => {
 
   t.true(tagAtlas.find('hello world') === undefined, `[[hello world] should be undefined`);
 });
+
+test('splitTitle correctly splits words', t => {
+  const tagAtlas = new TagAtlas({slugify});
+  const checks = {
+    '11ty': '11ty',
+    'DOS': 'DOS',
+    'JS': 'JS',
+    'JavaScript': 'Java Script',
+    'HelloWorld': 'Hello World',
+    'GameDevelopment': 'Game Development',
+    'PDFSplitAndMergeSamples': 'PDF Split And Merge Samples',
+  };
+
+  for (const [input, expected] of Object.entries(checks)) {
+    const result = tagAtlas.splitTitle(input);
+    t.true(result === expected, `[${result}] is not "${expected}"`);
+  }
+});
+
+test('strToSlug works correctly when tile atlas given no config options', t => {
+  const tagAtlas = new TagAtlas({slugify});
+  const fn = strToSlug(tagAtlas);
+  const checks = {
+    '11ty': '11ty',
+    'DOSCember': 'dos-cember',
+    'DOS': 'dos',
+    'MS-DOS': 'ms-dos',
+  };
+
+  for (const [input, output] of Object.entries(checks)) {
+    t.true(fn(input) === output, `[${fn(input)}] is not "${output}"`);
+  }
+});
+
+test('slugToStr works correctly when tile atlas given no config options', t => {
+  const tagAtlas = new TagAtlas({slugify});
+  const fn = slugToStr(tagAtlas);
+  const checks = {
+    '11ty': '11ty',
+    'doscember': 'Doscember',
+    'dos': 'Dos',
+    'js': 'Js',
+    'php': 'Php',
+    'javascript': 'Javascript',
+    'ms-dos': 'Ms Dos', // heh, but it's not aware that MS-DOS is an ignored input
+  };
+
+  for (const [input, output] of Object.entries(checks)) {
+    t.true(fn(input) === output, `[${fn(input)}] is not "${output}"`);
+  }
+});
+
+test('slugToStr returns the split title from strToSlug', t => {
+  const tagAtlas = new TagAtlas({slugify});
+  const stringToSlug = strToSlug(tagAtlas);
+  const slugToString = slugToStr(tagAtlas);
+
+  // Because tagAtlas has been taught JavaScript before javascript, it should register the lowercase variety
+  // as linked to the camelcase.
+  const checks = {
+    'JavaScript': 'Java Script',
+    'javascript': 'Java Script',
+  };
+
+  for (const [input, expected] of Object.entries(checks)) {
+    const slug = stringToSlug(input);
+    const result = slugToString(slug);
+    t.true(result === expected, `[${result}] does not match "${expected}"`);
+  }
+})
+
+test('lowercase variants added on creation', t => {
+  const tagAtlas = new TagAtlas({slugify});
+
+  // This should create two records, one for "JavaScript" and one for "javascript".
+  tagAtlas.findOrCreateByTitle('JavaScript');
+  t.true(tagAtlas.find('JavaScript').title === tagAtlas.find('javascript').title);
+
+  tagAtlas.findOrCreateByTitle('DOS');
+  t.true(tagAtlas.find('DOS').title === tagAtlas.find('dos').title);
+})
